a37946d79466ce295594a37bddd640024a394e02
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2018-12-06 16:43:12 +0000

    update img

--- src/components/index/hero.js
+++ src/components/index/hero.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import humberBridge from '../../images/humber_bridge.jpg';
 
 class Hero extends React.Component {
   render(){
     return (
       <div className="hero">
-
+        <img src={humberBridge} alt="humber bridge"></img>
       </div>
     );
   }
